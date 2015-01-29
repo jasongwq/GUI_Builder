c20df4a5a3e38d24c6422ef973da5b10c5dda523
jasongwq <jasongwq@gmail.com>
2015-01-29 15:24:58 +0800

    2

--- __init__.py
+++ __init__.py
@@ -39,61 +39,61 @@ def get_gb2312_pix(gb2312_code, w, h, usr_font):
         d_usr.text((0, 0), u" ", (0), font=usr_font)
     return get_pix(image)
 def dot_matrix(hz_in):
-    truetypefile = 'simsun.ttc'
+    truetypefile = 'font\simsun.ttc'
     font_width = 16
     font_height = 16
-    outfilename = 'dot_matrix.font'
+#    outfilename = 'dot_matrix.font'
     usr_font = ImageFont.truetype(truetypefile, font_height)
-    with open(outfilename, 'wb') as outfile:
+#    with open(outfilename, 'wb') as outfile:
 #        hz_in=u'美的吃喝拉撒美的吃喝拉撒'
-        hz_in=list(hz_in)
-        hz_in=list(set(hz_in))
-        
+    hz_in=list(hz_in)
+    hz_in=list(set(hz_in))
+    
 #        hz_list.sort()
-        str=[]
-        str.append([])
-        str.append([])
-        str.append([])
-        str[1].append("void *p%s_%sx%s[]={"%(truetypefile[:-4],font_width, font_height))
-        str[2].append("u16 GBK_code_%s_%sx%s[]={"%(truetypefile[:-4],font_width, font_height))
-        hz_list=[]
-        for a in range(len(hz_in)):
-            hz_list.append(unicode(hz_in[a]).encode('gbk'))
-        hz_list.sort();
-        for a in hz_list:
-            if len(a)>1:
-                data = get_gb2312_pix(chr(ord(a[0])) + chr(ord(a[1])), font_width, font_height, usr_font)
-                str[0].append("//%s\n"%a)
-                str[0].append("char ot_%s[]={"%(hex(ord(a[0])*256+(ord(a[1])))))
-                for x in range(0, len(data), 8):
-                    if x>0:
-                        str[0].append(",")
-                    str[0].append("%s"%(hex(int(data.to01()[x:x+8], 2))))
-                str[0].append("};\n")
-                if len(str[1])==1:
-                    str[1].append("\n(void *)ot_%s"%(hex(ord(a[0])*256+(ord(a[1])))))
-                else:
-                    str[1].append(",\n(void *)ot_%s"%(hex(ord(a[0])*256+(ord(a[1])))))
-                if len(str[2])==1:
-                    str[2].append("%s"%(hex(ord(a[0])*256+(ord(a[1])))))
-                else:
-                    str[2].append(",%s"%(hex(ord(a[0])*256+(ord(a[1])))))
-        str[1].append("};\n")
-        str[2].append("};\n")
-        
-        str[2].append("Font User_Font_%s_%sx%s={\n"%(truetypefile[:-4],font_width, font_height))
-        str[2].append("    GBK_code_%s_%sx%s,\n"%(truetypefile[:-4],font_width, font_height))
-        str[2].append("    p%s_%sx%s,\n"%(truetypefile[:-4],font_width, font_height))
-        str[2].append("    %s,\n"%font_width)
-        str[2].append("    sizeof(GBK_code_%s_%sx%s)\n"%(truetypefile[:-4],font_width, font_height))
-        str[2].append("};")
-        
-        pkl_filename='font_Mat.h'
-        file_object = open(pkl_filename, 'w')
-        file_object.writelines(str[0])
-        file_object.writelines(str[1])
-        file_object.writelines(str[2])
-        file_object.close()
+    str=[]
+    str.append([])
+    str.append([])
+    str.append([])
+    str[1].append("void *p%s_%sx%s[]={"%(truetypefile[:-4],font_width, font_height))
+    str[2].append("u16 GBK_code_%s_%sx%s[]={"%(truetypefile[:-4],font_width, font_height))
+    hz_list=[]
+    for a in range(len(hz_in)):
+        hz_list.append(unicode(hz_in[a]).encode('gbk'))
+    hz_list.sort();
+    for a in hz_list:
+        if len(a)>1:
+            data = get_gb2312_pix(chr(ord(a[0])) + chr(ord(a[1])), font_width, font_height, usr_font)
+            str[0].append("//%s\n"%a)
+            str[0].append("char ot_%s[]={"%(hex(ord(a[0])*256+(ord(a[1])))))
+            for x in range(0, len(data), 8):
+                if x>0:
+                    str[0].append(",")
+                str[0].append("%s"%(hex(int(data.to01()[x:x+8], 2))))
+            str[0].append("};\n")
+            if len(str[1])==1:
+                str[1].append("\n(void *)ot_%s"%(hex(ord(a[0])*256+(ord(a[1])))))
+            else:
+                str[1].append(",\n(void *)ot_%s"%(hex(ord(a[0])*256+(ord(a[1])))))
+            if len(str[2])==1:
+                str[2].append("%s"%(hex(ord(a[0])*256+(ord(a[1])))))
+            else:
+                str[2].append(",%s"%(hex(ord(a[0])*256+(ord(a[1])))))
+    str[1].append("};\n")
+    str[2].append("};\n")
+    
+    str[2].append("Font User_Font_%s_%sx%s={\n"%(truetypefile[:-4],font_width, font_height))
+    str[2].append("    GBK_code_%s_%sx%s,\n"%(truetypefile[:-4],font_width, font_height))
+    str[2].append("    p%s_%sx%s,\n"%(truetypefile[:-4],font_width, font_height))
+    str[2].append("    %s,\n"%font_width)
+    str[2].append("    sizeof(GBK_code_%s_%sx%s)\n"%(truetypefile[:-4],font_width, font_height))
+    str[2].append("};")
+    
+    pkl_filename2='buildc\\font_Mat.h'
+    file_object = open(pkl_filename2, 'w')
+    file_object.writelines(str[0])
+    file_object.writelines(str[1])
+    file_object.writelines(str[2])
+    file_object.close()
     
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -303,7 +303,7 @@ class Example(QtGui.QMainWindow):
         self.setGeometry(300, 300, 800, 600)
         self.statusBar().showMessage('Ready')
         self.setWindowTitle(_fromUtf8("窗口编辑"))
-        self.setWindowIcon(QtGui.QIcon('ico.png'))
+        self.setWindowIcon(QtGui.QIcon('ico\ico.png'))
         self.show()
         self.center()
                 
@@ -489,7 +489,8 @@ class Example(QtGui.QMainWindow):
             text=_translate("Dialog", col.name(), None)
             self.lineEdit.setText("%s"%text[1:])
     def onpushButton_4Clicked(self):
-        pkl_filename=QtGui.QFileDialog.getSaveFileName(self)
+#        pkl_filename=QtGui.QFileDialog.getSaveFileName(self)
+        pkl_filename='buildc\UI_User.cpp'
         print pkl_filename
         k="%s"%pkl_filename
 #        k=k[-4:]
